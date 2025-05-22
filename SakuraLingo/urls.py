99ed59857fe2cb3d158a0dcab02d65fd8a0e60ac
GudUgne <60691717+GudUgne@users.noreply.github.com>
2025-05-22 15:27:13 +0300

    small upd

--- SakuraLingo/urls.py
+++ SakuraLingo/urls.py
@@ -1,22 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    RegisterView,
-    LoginView,
-    ExerciseMatchListCreateView,
-    MyGroupsView,
-    CreateGroupView,
-    RequestToJoinGroup,
-    PendingRequestsView,
-    ApproveRequestView, CurrentUserView, MyPendingRequestsView,
-SendMessageView, GetConversationView, UserListView, SearchGroupsView, CancelRequestView, GroupDetailView,
-    ExerciseMatchOptionsListCreateView,ExerciseMultiChoiceView,
-    ExerciseFreetextListCreateView, TeacherReviewSubmissionView, PendingSubmissionsView,
-ExerciseFreetextDetailView,
-)
+# from .views import (
+#     RegisterView,
+#     LoginView,
+#     ExerciseMatchListCreateView,
+#     MyGroupsView,
+#     CreateGroupView,
+#     RequestToJoinGroup,
+#     PendingRequestsView,
+#     ApproveRequestView, CurrentUserView, MyPendingRequestsView,
+# SendMessageView, GetConversationView, UserListView, SearchGroupsView, CancelRequestView, GroupDetailView,
+#     ExerciseMatchOptionsListCreateView,ExerciseMultiChoiceView,
+#     ExerciseFreetextListCreateView, TeacherReviewSubmissionView, PendingSubmissionsView,
+# ExerciseFreetextDetailView,
+# )
 
+from .views import *
 router = DefaultRouter()
-# router.register(r'exercise-match', ExerciseMatchViewSet, basename='exercise-match')
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -24,9 +24,6 @@ urlpatterns = [
     path('users/login/', LoginView.as_view(), name='login'),
     path('users/', UserListView.as_view(), name='user_list'),
     path('users/me/', CurrentUserView.as_view(), name='current-user'),
-
-    # path('exercise-match/', ExerciseMatchListCreateView.as_view(), name='exercise-match'),
-    # path('exercise-match-options/', ExerciseMatchOptionsListCreateView.as_view(), name='exercise-match-options'),
 
     path('exercise-match/', ExerciseMatchListCreateView.as_view(), name='exercise-match-list'),
     path('exercise-match/<int:match_id>/', ExerciseMatchListCreateView.as_view(), name='exercise-match-detail'),
@@ -39,7 +36,7 @@ urlpatterns = [
 
     path('exercise-freetext/', ExerciseFreetextListCreateView.as_view(), name='exercise-freetext-list'),
     path('exercise-freetext/<int:pk>/', ExerciseFreetextDetailView.as_view(), name='exercise-freetext-detail'),
-    # path('freetext-submission/', FreetextSubmissionView.as_view(), name='freetext-submission'),
+
     path('freetext-review/<int:submission_id>/', TeacherReviewSubmissionView.as_view(), name='freetext-review'),
     path('freetext-pending/', PendingSubmissionsView.as_view(), name='freetext-pending'),
 
@@ -50,16 +47,16 @@ urlpatterns = [
     path('groups/requests/', PendingRequestsView.as_view(), name='pending-requests'),
     path('groups/my-pending-requests/', MyPendingRequestsView.as_view(), name='my-pending-requests'),
     path('groups/<int:group_id>/approve/<int:student_id>/', ApproveRequestView.as_view(), name='approve-student'),
-path(
-  'groups/<int:group_id>/',
-  GroupDetailView.as_view(),
-  name='group-detail'
-),
-path(
-  'groups/<int:group_id>/requests/<int:request_id>/',
-  CancelRequestView.as_view(),
-  name='cancel-request'
-),
+    path(
+      'groups/<int:group_id>/',
+      GroupDetailView.as_view(),
+      name='group-detail'
+    ),
+    path(
+      'groups/<int:group_id>/requests/<int:request_id>/',
+      CancelRequestView.as_view(),
+      name='cancel-request'
+    ),
 
     path('messages/send/',SendMessageView.as_view(), name='send_message'),
     path('messages/conversation/', GetConversationView.as_view(), name='get_conversation'),
